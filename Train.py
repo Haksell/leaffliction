8949b46e49980c5db3ac4138a392dd6370972250
Axel Brisse <axel.brisse@gmail.com>
2024-05-18 22:43:26 +0200

    more data augmentations = 0.9845

--- Train.py
+++ Train.py
@@ -1,4 +1,4 @@
-# [0.5941, 0.9445, 0.9619, 0.9747, 0.9563, 0.9758, 0.9747, 0.9748, 0.9764, 0.9747]
+# [0.7043, 0.9407, 0.9546, 0.9698, 0.9726, 0.9794, 0.9837, 0.9823, 0.9839, 0.9845]
 
 import pandas as pd
 import statistics
@@ -6,11 +6,12 @@ from tensorflow import keras
 
 NUM_CLASSES = 4  # TODO: automatically from image_dataset_from_directory
 IMAGE_SIZE = 64
+CROP_SIZE = round(IMAGE_SIZE * 0.9)
 EPOCHS = 100
 MEAN_EPOCHS = 10
 assert EPOCHS % MEAN_EPOCHS == 0
 
-ds_train, ds_rest = keras.preprocessing.image_dataset_from_directory(
+ds_train, ds_valid = keras.preprocessing.image_dataset_from_directory(
     "../input/images/images/apple",
     labels="inferred",
     label_mode="categorical",
@@ -18,21 +19,22 @@ ds_train, ds_rest = keras.preprocessing.image_dataset_from_directory(
     interpolation="nearest",
     batch_size=64,
     shuffle=True,
-    validation_split=0.3,
+    validation_split=0.2,
     subset="both",
     seed=42,
 )
-val_size = int(0.5 * len(ds_rest))
-ds_valid = ds_rest.take(val_size)
-ds_test = ds_rest.skip(val_size)
 
 model = keras.Sequential(
     [
         keras.layers.InputLayer(input_shape=[IMAGE_SIZE, IMAGE_SIZE, 3]),
         # Data Augmentation
         keras.layers.RandomContrast(factor=0.1),
-        keras.layers.RandomFlip(mode="horizontal"),
+        keras.layers.RandomFlip(mode="horizontal_and_vertical"),
         keras.layers.RandomRotation(factor=0.1),
+        keras.layers.RandomZoom(height_factor=0.1, width_factor=0.1),
+        keras.layers.RandomTranslation(height_factor=0.1, width_factor=0.1),
+        keras.layers.RandomBrightness(factor=0.1),
+        keras.layers.RandomCrop(height=CROP_SIZE, width=CROP_SIZE),
         # Block One
         keras.layers.BatchNormalization(),
         keras.layers.Conv2D(64, kernel_size=3, activation="relu", padding="same"),
@@ -55,6 +57,7 @@ model = keras.Sequential(
     ]
 )
 
+
 model.compile(
     optimizer=keras.optimizers.Adam(epsilon=0.01),
     loss="categorical_crossentropy",
@@ -64,8 +67,13 @@ model.compile(
 checkpoint = keras.callbacks.ModelCheckpoint(
     "best_model.keras", save_best_only=True, monitor="val_loss", mode="min"
 )
+lr_scheduler = keras.callbacks.LearningRateScheduler(lambda _, lr: lr * 0.97)
+
 history = model.fit(
-    ds_train, validation_data=ds_valid, epochs=EPOCHS, callbacks=[checkpoint]
+    ds_train,
+    validation_data=ds_valid,
+    epochs=EPOCHS,
+    callbacks=[checkpoint, lr_scheduler],
 )
 model.load_weights("best_model.keras")
 
@@ -79,6 +87,3 @@ print(
         for i in range(0, EPOCHS, MEAN_EPOCHS)
     ]
 )
-
-test_loss, test_accuracy = model.evaluate(ds_test)
-print(f"Test accuracy: {test_accuracy:.4f}")

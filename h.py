d06e62686044fe3bfb2ab650a8c13d59a527a139
Axel Brisse <axel.brisse@gmail.com>
2024-05-20 08:29:33 +0200

    histogram good but only rgb

--- h.py
+++ h.py
@@ -1,40 +1,42 @@
+import sys
 import matplotlib.pyplot as plt
 from plantcv import plantcv as pcv
 
 BINS = 64
 
-img, _, _ = pcv.readimage(filename="garbage.jpg")
-_, hist_data = pcv.visualize.histogram(img=img, hist_data=True, bins=BINS)
-blue, green, red = hist_data[:BINS], hist_data[BINS : 2 * BINS], hist_data[2 * BINS :]
-print(blue)
-print(green)
-print(red)
 
-fig, ax = plt.subplots()
+def plot_histogram(ax, data, name):
+    ax.plot(
+        data["pixel intensity"],
+        data["proportion of pixels (%)"],
+        label=name.title(),
+        color=name,
+    )
 
-# Plot histograms
-ax.plot(
-    blue["pixel intensity"],
-    blue["proportion of pixels (%)"],
-    label="Blue",
-    color="blue",
-)
-ax.plot(
-    green["pixel intensity"],
-    green["proportion of pixels (%)"],
-    label="Green",
-    color="green",
-)
-ax.plot(
-    red["pixel intensity"], red["proportion of pixels (%)"], label="Red", color="red"
-)
 
-# Labeling
-ax.set_xlabel("Pixel Intensity")
-ax.set_ylabel("Histogram Count")
-ax.set_title("Color Channel Intensity Histograms")
-ax.legend()
+def main(filename):
+    img, _, _ = pcv.readimage(filename)
+    _, hist_data = pcv.visualize.histogram(img=img, hist_data=True, bins=BINS)
+    blue, green, red = (
+        hist_data[:BINS],
+        hist_data[BINS : 2 * BINS],
+        hist_data[2 * BINS :],
+    )
 
-# Show plot
-plt.tight_layout()
-plt.show()
+    _, ax = plt.subplots()
+
+    plot_histogram(ax, blue, "blue")
+    plot_histogram(ax, green, "green")
+    plot_histogram(ax, red, "red")
+    ax.set_xlabel("Pixel Intensity")
+    ax.set_ylabel("Proportion of Pixels (%)")
+    ax.set_title("Color Channel Intensity Histograms")
+    ax.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+
+if __name__ == "__main__":
+    # TODO: argparse
+    main(sys.argv[1])

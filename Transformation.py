bbdada4cad91e849d199ebcbb2d081c0fcc9ef66
Axel Brisse <axel.brisse@gmail.com>
2024-05-21 08:52:11 +0200

    push

--- Transformation.py
+++ Transformation.py
@@ -1,40 +1,63 @@
 import argparse
+import os
+import shutil
 import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from plantcv import plantcv as pcv
 
 
-def pseudolandmarks_place_dots(img, dots, color):
-    for pos in dots:
-        cv2.circle(
-            img,
-            tuple(map(int, pos[0])),
-            3,
-            color,
-            -1,
-            lineType=cv2.LINE_AA,
-        )
+def transformation_masked(img, mask):
+    return pcv.apply_mask(img, mask, "black")
 
 
-# TODO: better pseudolandmarks
-def pseudolandmarks(img, mask):
-    left, right, center = pcv.homology.y_axis_pseudolandmarks(img=img, mask=mask)
-    img = np.copy(img)
-    pseudolandmarks_place_dots(img, left, (255, 0, 0))
-    pseudolandmarks_place_dots(img, right, (255, 0, 255))
-    pseudolandmarks_place_dots(img, center, (0, 79, 255))
+def transformation_canny(img, mask):
+    return pcv.canny_edge_detect(pcv.apply_mask(img, mask, "black"), sigma=1.3)
+
+
+def transformation_background(img, mask):
+    return pcv.apply_mask(img, 255 - mask, "black")
+
+
+def transformation_analyze(img, mask):
+    roi = pcv.roi.rectangle(img=img, x=0, y=0, h=img.shape[0], w=img.shape[1])
+    labeled_mask = pcv.roi.filter(mask=mask, roi=roi, roi_type="partial")
+    return pcv.analyze.size(img, labeled_mask)
+
+
+def transformation_original(img, _):
     return img
 
 
-def contrast_limited_adaptive_histogram_equalization(img):
+# TODO: better pseudolandmarks
+def transformation_pseudolandmarks(img, mask):
+    def pseudolandmarks_place_dots(dots, color):
+        for pos in dots:
+            cv2.circle(
+                cpy,
+                tuple(map(int, pos[0])),
+                3,
+                color,
+                -1,
+                lineType=cv2.LINE_AA,
+            )
+
+    left, right, center = pcv.homology.y_axis_pseudolandmarks(img=img, mask=mask)
+    cpy = np.copy(img)
+    pseudolandmarks_place_dots(left, (255, 0, 0))
+    pseudolandmarks_place_dots(right, (255, 0, 255))
+    pseudolandmarks_place_dots(center, (0, 79, 255))
+    return cpy
+
+
+def transformation_clahe(img, _):
     light, a, b = cv2.split(cv2.cvtColor(img, cv2.COLOR_BGR2LAB))
     light = cv2.createCLAHE(clipLimit=3.0, tileGridSize=(8, 8)).apply(light)
     return cv2.cvtColor(cv2.merge((light, a, b)), cv2.COLOR_LAB2BGR)
 
 
 def transformations(img):
-    clahe = contrast_limited_adaptive_histogram_equalization(img)
+    clahe = transformation_clahe(img, None)
     bin_img = pcv.threshold.dual_channels(
         rgb_img=clahe,
         x_channel="a",
@@ -42,17 +65,14 @@ def transformations(img):
         points=[(75, 75), (130, 145)],
     )
     mask = pcv.fill_holes(pcv.fill(bin_img=bin_img, size=50))
-    masked = pcv.apply_mask(img, mask, "black")
-    roi = pcv.roi.rectangle(img=img, x=0, y=0, h=img.shape[0], w=img.shape[1])
-    labeled_mask = pcv.roi.filter(mask=mask, roi=roi, roi_type="partial")
 
     images = [
-        (masked, "Mask"),
-        (pcv.canny_edge_detect(masked, sigma=1.3), "Canny edge detection"),
-        (pcv.apply_mask(img, 255 - mask, "black"), "Background"),
-        (pcv.analyze.size(img, labeled_mask), "Analyze object"),
-        (img, "Original"),
-        (pseudolandmarks(img, mask), "Pseudolandmarks"),
+        (transformation_masked(img, mask), "Mask"),
+        (transformation_canny(img, mask), "Canny edge detection"),
+        (transformation_background(img, mask), "Background"),
+        (transformation_analyze(img, mask), "Analyze object"),
+        (transformation_original(img, mask), "Original"),
+        (transformation_pseudolandmarks(img, mask), "Pseudolandmarks"),
         (pcv.hist_equalization(pcv.rgb2gray(img)), "Histogram equalization"),
         (pcv.gaussian_blur(img, ksize=(11, 11)), "Gaussian blur"),
         (clahe, "CLAHE"),
@@ -151,7 +171,12 @@ def handle_file(filename):
 
 
 def handle_directory(src, dst, transformation):
-    raise NotImplementedError
+    assert os.path.isdir(src)
+    shutil.rmtree(dst, ignore_errors=True)
+    os.makedirs(dst, exist_ok=True)
+    assert all(map(os.path.isfile, os.listdir(src)))
+    for file in os.listdir(src):
+        print(file)
 
 
 def main():

5a4257dd5556ce5f74467add7d91991b5ad5a0b1
Axel Brisse <axel.brisse@gmail.com>
2024-05-21 05:56:25 +0200

    .

--- Analyze.py
+++ Analyze.py
@@ -2,15 +2,11 @@ import sys
 from plantcv import plantcv as pcv
 import matplotlib.pyplot as plt
 
-pcv.params.dpi = 100
-pcv.params.text_size = 20
-pcv.params.text_thickness = 20
-
 
 def process_and_analyze_image(filename):
     img, _, _ = pcv.readimage(filename=filename)
 
-    thresh1 = pcv.threshold.dual_channels(
+    bin_img = pcv.threshold.dual_channels(
         rgb_img=img,
         x_channel="a",
         y_channel="b",
@@ -18,22 +14,24 @@ def process_and_analyze_image(filename):
         above=True,
     )
 
-    mask = pcv.fill(bin_img=thresh1, size=50)
+    mask = pcv.fill(bin_img=bin_img, size=50)
     mask = pcv.fill_holes(mask)
-    roi1 = pcv.roi.rectangle(img=img, x=0, y=0, h=img.shape[0], w=img.shape[1])
-    labeled_mask = pcv.roi.filter(mask=mask, roi=roi1, roi_type="partial")
-    analysis_image = pcv.analyze.size(img=img, labeled_mask=labeled_mask)
-    return analysis_image
+    roi = pcv.roi.rectangle(img=img, x=0, y=0, h=img.shape[0], w=img.shape[1])
+    labeled_mask = pcv.roi.filter(mask=mask, roi=roi, roi_type="partial")
+    return pcv.analyze.size(img=img, labeled_mask=labeled_mask)
 
 
-filenames = sys.argv[1:5]
-fig, axs = plt.subplots(2, 2)
+def main():
+    _, axs = plt.subplots(2, 2)
+    for i, filename in enumerate(sys.argv[1:5]):
+        row = i // 2
+        col = i % 2
+        axs[row, col].axis("off")
+        axs[row, col].imshow(process_and_analyze_image(filename))
+        axs[row, col].set_title(f"Image {i + 1}")
+    plt.tight_layout()
+    plt.show()
 
-for i, filename in enumerate(sys.argv[1:5]):
-    row = i // 2
-    col = i % 2
-    axs[row, col].imshow(process_and_analyze_image(filename))
-    axs[row, col].set_title(f"Image {i + 1}")
 
-plt.tight_layout()
-plt.show()
+if __name__ == "__main__":
+    main()

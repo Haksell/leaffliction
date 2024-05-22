ba5f0fb3f406ae471c90811105b37edcac2276b6
Axel Brisse <axel.brisse@gmail.com>
2024-05-23 00:13:37 +0200

    post-push cleaning

---
+++ download_dataset.sh
@@ -0,0 +1,19 @@
+#/bin/sh
+
+wget https://cdn.intra.42.fr/document/document/17547/leaves.zip
+unzip leaves.zip
+rm -rf leaves.zip
+
+chmod 755 -R images
+mkdir -p images/apple
+mkdir -p images/grape
+
+mv images/Apple_Black_rot images/apple/black_rot
+mv images/Apple_healthy images/apple/healthy
+mv images/Apple_rust images/apple/rust
+mv images/Apple_scab images/apple/scab
+
+mv images/Grape_Black_rot images/grape/black_rot
+mv images/Grape_Esca images/grape/esca
+mv images/Grape_healthy images/grape/healthy
+mv images/Grape_spot images/grape/spot

234f09fa93a7b001bc0b6a61322ffebb1730d060
Axel Brisse <axel.brisse@gmail.com>
2024-05-23 00:19:03 +0200

    ooga

--- download_dataset.sh
+++ download_dataset.sh
@@ -5,14 +5,14 @@ unzip leaves.zip
 rm -rf leaves.zip
 
 chmod 755 -R images
-mkdir -p images/apple
-mkdir -p images/grape
 
+mkdir -p images/apple
 mv images/Apple_Black_rot images/apple/black_rot
 mv images/Apple_healthy images/apple/healthy
 mv images/Apple_rust images/apple/rust
 mv images/Apple_scab images/apple/scab
 
+mkdir -p images/grape
 mv images/Grape_Black_rot images/grape/black_rot
 mv images/Grape_Esca images/grape/esca
 mv images/Grape_healthy images/grape/healthy
